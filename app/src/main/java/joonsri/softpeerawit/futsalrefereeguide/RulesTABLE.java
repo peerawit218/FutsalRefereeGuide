222dd9c55a87da3e5dfcd867fc86fb522df0ab1f
Peerawit218 <soft_lying@hotmail.com>
2016-06-06 10:29:36 +0700

    ติดdoInback

--- app/src/main/java/joonsri/softpeerawit/futsalrefereeguide/RulesTABLE.java
+++ app/src/main/java/joonsri/softpeerawit/futsalrefereeguide/RulesTABLE.java
@@ -1,24 +1,41 @@
 package joonsri.softpeerawit.futsalrefereeguide;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by SOFT on 06/04/2016.
+ * Created by SOFT on 06/05/2016.
  */
-public class RulesTABLE {
+public class rulesTABLE {
 
     //Explicit
     private MyOpenHelper myOpenHelper;
-    private SQLiteDatabase writeSQLite, readSQLite;
+    private SQLiteDatabase sqLiteDatabase;
 
-    public RulesTABLE(Context context) {
+    public static final String rules_table = "rulesTABLE";
+    public static final String column_id = "id_rules";
+    public static final String column_rules = "rules";
+    public static final String column_title = "title";
+    public static final String column_content = "content";
+    public static final String column_image = "image";
 
+    public rulesTABLE(Context context) {
         myOpenHelper = new MyOpenHelper(context);
-        writeSQLite = myOpenHelper.getWritableDatabase();
-        readSQLite = myOpenHelper.getReadableDatabase();
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
+    }//Constructor
 
-    }//constructor
+    public long addNewRules(String strRules,
+                            String strTitle,
+                            String strContent,
+                            String strImage) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_rules, strRules);
+        contentValues.put(column_title, strTitle);
+        contentValues.put(column_content, strContent);
+        contentValues.put(column_image, strImage);
+        return sqLiteDatabase.insert(rules_table, null, contentValues);
+    }
 
-}//Main class
+
+}//main Class
--- app/src/main/java/joonsri/softpeerawit/futsalrefereeguide/VideoTABLE.java
+++ app/src/main/java/joonsri/softpeerawit/futsalrefereeguide/VideoTABLE.java
@@ -1,23 +1,36 @@
 package joonsri.softpeerawit.futsalrefereeguide;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 
 /**
- * Created by SOFT on 06/04/2016.
+ * Created by SOFT on 06/05/2016.
  */
-public class VideoTABLE {
+public class videoTABLE {
 
-    //Explicit
     private MyOpenHelper myOpenHelper;
-    private SQLiteDatabase writeSQLite, readSQLite;
+    private SQLiteDatabase sqLiteDatabase;
 
-    public VideoTABLE(Context context) {
+    public static final String video_table = "videoTABLE";
+    public static final String column_id = "id_video";
+    public static final String column_name = "name";
+    public static final String column_video = "video";
 
+
+    public videoTABLE(Context context) {
         myOpenHelper = new MyOpenHelper(context);
-        writeSQLite = myOpenHelper.getWritableDatabase();
-        readSQLite = myOpenHelper.getReadableDatabase();
+        sqLiteDatabase = myOpenHelper.getWritableDatabase();
+    }//Constructor
 
-    }//constructor
+    public long addNewVideo(String strName,
+                            String strVideo) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_name, strName);
+        contentValues.put(column_video, strVideo);
+        return sqLiteDatabase.insert(video_table, null, contentValues);
+    }
+
 
 }//Main Class
